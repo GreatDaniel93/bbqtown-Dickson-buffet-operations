1d5356c0b208aad692fe25916574119ddd6730a2
GreatDaniel93 <danielchen993@gmail.com>
2026-08-30 16:18:09 +1000

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main style={{ width: "100vw", height: "100dvh", margin: 0, padding: 0 }}>
+      <iframe title="BBQ Town Buffet Operations" src="/ops.html" style={{ width: "100%", height: "100%", border: 0, display: "block" }} />
+    </main>
+  );
+}
