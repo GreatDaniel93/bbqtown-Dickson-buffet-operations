44333bbded2b18652ea4057cb10c8157f5fc0378
GreatDaniel93 <danielchen993@gmail.com>
2026-09-07 18:47:29 +1000

    Use permanent redirect to public booking homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from "next/navigation";
+import { permanentRedirect } from "next/navigation";
 
 export default function Page() {
-  redirect("/book.html");
+  permanentRedirect("/book.html");
 }
