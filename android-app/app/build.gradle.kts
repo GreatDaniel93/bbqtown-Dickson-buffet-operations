bca7c0587c80ce19c566acdbc3f9e4d252d900f3
GreatDaniel93 <danielchen993@gmail.com>
2026-09-08 20:23:56 +1000

    Align Android Java and Kotlin targets

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -15,10 +15,19 @@ android {
         versionName = "1.0.0"
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+}
+
+kotlin {
+    jvmToolchain(17)
 }
