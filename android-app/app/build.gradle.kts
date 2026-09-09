8437f23985f2f6df2035c175a9ba4131a69d30db
GreatDaniel93 <danielchen993@gmail.com>
2026-09-09 11:51:51 +1000

    Bump vector logo build to alpha5.5

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bbqtown.dickson.ops"
         minSdk = 24
         targetSdk = 35
-        versionCode = 15
-        versionName = "2.0.0-alpha5.4"
+        versionCode = 16
+        versionName = "2.0.0-alpha5.5"
     }
 
     buildFeatures {
