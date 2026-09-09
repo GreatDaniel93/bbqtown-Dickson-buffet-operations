5f0d2a0eb961e70a178e118c12311bb10122e7e6
GreatDaniel93 <danielchen993@gmail.com>
2026-09-09 12:21:02 +1000

    Bump traced-logo app to alpha5.7

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bbqtown.dickson.ops"
         minSdk = 24
         targetSdk = 35
-        versionCode = 17
-        versionName = "2.0.0-alpha5.6"
+        versionCode = 18
+        versionName = "2.0.0-alpha5.7"
     }
 
     buildFeatures {
