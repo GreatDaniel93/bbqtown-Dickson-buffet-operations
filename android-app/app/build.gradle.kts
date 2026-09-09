14377b2c2d0f13d795e309bf68773a5e9fdb9842
GreatDaniel93 <danielchen993@gmail.com>
2026-09-09 11:00:22 +1000

    Use native logo resource and bump alpha5.4

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bbqtown.dickson.ops"
         minSdk = 24
         targetSdk = 35
-        versionCode = 14
-        versionName = "2.0.0-alpha5.3"
+        versionCode = 15
+        versionName = "2.0.0-alpha5.4"
     }
 
     buildFeatures {
