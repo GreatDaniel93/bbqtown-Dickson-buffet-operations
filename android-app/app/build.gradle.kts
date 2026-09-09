2713351d9a7f986e684e492557e8d59c125cc56f
GreatDaniel93 <danielchen993@gmail.com>
2026-09-09 14:58:48 +1000

    Bump segmented kitchen board build to alpha5.9

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bbqtown.dickson.ops"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.0.0-alpha5.8"
+        versionCode = 20
+        versionName = "2.0.0-alpha5.9"
     }
 
     buildFeatures {
