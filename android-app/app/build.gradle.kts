d069c8b6a98accb75ddaecd4d592bf13c3cdc9b9
GreatDaniel93 <danielchen993@gmail.com>
2026-09-15 12:54:55 +1000

    Bump incremental sync build to alpha5.10

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.bbqtown.dickson.ops"
         minSdk = 24
         targetSdk = 35
-        versionCode = 20
-        versionName = "2.0.0-alpha5.9"
+        versionCode = 21
+        versionName = "2.0.0-alpha5.10"
     }
 
     buildFeatures {
